f9cded8d2900fc50f3561e4365a5e496820c095f
awyyyn <alwin.puche16@gmail.com>
2024-01-20 21:53:15 +0800

    remove nx-prisma

--- apps/blog-app/src/graphql/graphql-server.ts
+++ apps/blog-app/src/graphql/graphql-server.ts
@@ -1,9 +1,9 @@
 import { ApolloServer } from '@apollo/server';
-import { buildSubgraphSchema } from '@apollo/subgraph';
 import { resolvers } from './resolvers';
 import { typeDefs } from './schema';
 // import { } from '@'
 
 export const server = new ApolloServer({
-  schema: buildSubgraphSchema({ typeDefs, resolvers }),
+  typeDefs,
+  resolvers,
 });
